52522e710f658aa033e92c83a4150d4ca0d126be
bhavesh joshi <joshbhavesh10@gmail.com>
2026-05-10 18:05:44 +0530

    more fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { BrowserRouter , Routes , Route, Navigate } from 'react-router-dom'
 import { DashBoard } from './Pages/DashBoard'
 import { LogIn } from './Pages/LogIn'
 import { SignUp } from './Pages/SignUp'
@@ -8,16 +8,19 @@ import { VideosDashBoard } from './Pages/Videos'
 import './App.css'
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return<>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashBoard/>}/>
+        <Route path="/" element={token ? <Navigate to="/dashboard"/> : <Navigate to="/login"/>}/>
         <Route path="/dashboard" element={<DashBoard/>}/>
         <Route path="/login" element={<LogIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/brain/:shareLink" element={<SharePage/>}/>
         <Route path="/user/tweets" element={<TweetsDashBoard/>}/>
         <Route path="/user/videos" element={<VideosDashBoard/>}/>
+        <Route path="*" element={<Navigate to="/login"/>}/>
       </Routes>
     </BrowserRouter>
   </>
