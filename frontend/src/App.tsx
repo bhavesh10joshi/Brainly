8a53e51c20204774269befedd092980cdc3fca5f
bhavesh joshi <joshbhavesh10@gmail.com>
2026-05-10 18:02:06 +0530

    Added base route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   return<>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<DashBoard/>}/>
         <Route path="/dashboard" element={<DashBoard/>}/>
         <Route path="/login" element={<LogIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
