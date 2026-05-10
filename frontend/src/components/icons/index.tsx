d17bbb23024a56c4108af23751037c8675005a51
bhavesh joshi <joshbhavesh10@gmail.com>
2026-05-10 17:57:12 +0530

    fix: icon file casing and barrel exports for Vercel

--- frontend/src/components/icons/index.tsx
+++ frontend/src/components/icons/index.tsx
@@ -8,3 +8,13 @@ export const IconSizes = {
     "md" : "size-7" ,
     "lg" : "size-9" ,
 }
+
+export { PlusIcon } from "./PlusIcon";
+export { ShareIcon } from "./ShareIcon";
+export { LogOutIcon } from "./LogoutIcon";
+export { TwitterIcon } from "./TwitterIcon";
+export { YoutubeIcon } from "./YoutubeIcon";
+export { CloseIcon } from "./CloseIcon";
+export { ClipBoardIcon } from "./ClipBoardIcon";
+export { DeleteIcon } from "./DeleteIcon";
+export { PageIcon } from "./PageIcon";
